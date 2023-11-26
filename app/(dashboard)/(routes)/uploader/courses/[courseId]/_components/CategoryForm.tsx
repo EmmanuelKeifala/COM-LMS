1fc38cbe3444e5519527963b65f3380bf76e816f
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2023-11-26 15:25:22 +0000

    eslint error in category form

--- app/(dashboard)/(routes)/uploader/courses/[courseId]/_components/CategoryForm.tsx
+++ app/(dashboard)/(routes)/uploader/courses/[courseId]/_components/CategoryForm.tsx
@@ -102,7 +102,7 @@ export const CategoryForm = ({
               render={({field}) => (
                 <FormItem>
                   <FormControl>
-                    <Combobox options={...options} {...field} />
+                    <Combobox options={options} {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
