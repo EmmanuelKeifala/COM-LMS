22e0c039a76c81a1ccccbb5e011ce126f4f05f08
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2023-11-29 08:15:11 +0000

    optimized the chapter view page

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -38,7 +38,7 @@ export const CourseEnrollButton = ({
       disabled={isLoading}
       size="sm"
       className="w-full md:w-auto">
-      Enroll for {formatPrice(price)}
+      Enroll
     </Button>
   );
 };
--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -71,7 +71,9 @@ const ChapterIdPage = async ({
           </div>
           <Separator />
           <div>
-            <Preview value={chapter.description!} />
+            {chapter.description != 'none' && (
+              <Preview value={chapter.description!} />
+            )}
           </div>
           <div>
             {!!attachments.length && (
