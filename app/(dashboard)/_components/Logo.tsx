6ff27841edaeeec186086075b354cc6744dfef70
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2023-12-14 02:31:37 +0000

    made the logo to redirect to the home route

--- app/(dashboard)/_components/Logo.tsx
+++ app/(dashboard)/_components/Logo.tsx
@@ -1,5 +1,10 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export const Logo = () => {
-  return <Image height={200} width={200} alt="logo" src="/logo1.png" />;
+  return (
+    <Link href={'/'}>
+      <Image height={200} width={200} alt="logo" src="/logo1.png" />;
+    </Link>
+  );
 };
