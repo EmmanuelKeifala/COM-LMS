dca47aeadc2522018899288c9a5f2448165543ff
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2023-12-17 19:41:27 +0000

    made the rating to be instantaneous

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/rating.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/rating.tsx
@@ -17,69 +17,112 @@ const Rating = ({chapterId, courseId, videoId}: RatingProps) => {
   const [rating, setRating] = useState<number | 0>(0);
   const {userId} = useAuth();
 
-  useEffect(() => {
-    const fetchVideoRating = async () => {
-      try {
-        const response = await fetch(
-          `/api/courses/${courseId}/chapters/${chapterId}/videoRating`,
-          {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-            },
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const fetchVideoRating = async () => {
+    try {
+      const response = await fetch(
+        `/api/courses/${courseId}/chapters/${chapterId}/videoRating`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
           },
-        );
+        },
+      );
 
-        if (!response.ok) {
-          throw new Error('Failed to fetch video rating');
-        }
-
-        const videoRatings = await response.json();
-        const filteredRatings = videoRatings.filter(
-          (rating: {videoId: string}) => rating.videoId === videoId,
-        );
-
-        const averageRating =
-          filteredRatings.length === 0
-            ? 0
-            : filteredRatings.reduce(
-                (sum: any, rating: {value: any}) =>
-                  (sum + rating.value) / filteredRatings.length,
-                0,
-              );
-        setRating(averageRating);
-      } catch (error) {
-        console.error('Error fetching video rating:', error);
+      if (!response.ok) {
+        throw new Error('Failed to fetch video rating');
       }
-    };
 
-    fetchVideoRating();
-  }, [chapterId, courseId, videoId]);
+      const videoRatings = await response.json();
+      const filteredRatings = videoRatings.filter(
+        (rating: {videoId: string}) => rating.videoId === videoId,
+      );
+
+      const averageRating =
+        filteredRatings.length === 0
+          ? 0
+          : filteredRatings.reduce(
+              (sum: any, rating: {value: any}) =>
+                (sum + rating.value) / filteredRatings.length,
+              0,
+            );
+      setRating(averageRating);
+    } catch (error) {
+      console.error('Error fetching video rating:', error);
+    }
+  };
 
   const handleChange = async (value: number) => {
     try {
+      // Send rating to the server
       await axios.post(
         `/api/courses/${courseId}/chapters/${chapterId}/videoRating`,
         {value, videoId, userId},
       );
 
+      // Update the local state immediately
+      const newRating = await getUpdatedRating();
+      setRating(newRating);
+
       toast.success('Video rated');
-      router.refresh();
     } catch {
       toast.error('Something went wrong');
     }
   };
+
+  const getUpdatedRating = async () => {
+    try {
+      const response = await fetch(
+        `/api/courses/${courseId}/chapters/${chapterId}/videoRating`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch updated video rating');
+      }
+
+      const videoRatings = await response.json();
+      const filteredRatings = videoRatings.filter(
+        (rating: {videoId: string}) => rating.videoId === videoId,
+      );
+
+      const averageRating =
+        filteredRatings.length === 0
+          ? 0
+          : filteredRatings.reduce(
+              (sum: any, rating: {value: any}) =>
+                (sum + rating.value) / filteredRatings.length,
+              0,
+            );
+
+      return averageRating;
+    } catch (error) {
+      console.error('Error fetching updated video rating:', error);
+      throw error;
+    }
+  };
+
+  useEffect(() => {
+    fetchVideoRating();
+  }, [chapterId, courseId, fetchVideoRating, videoId]);
+
   return (
     <div className="">
       <Rate
         count={5}
-        value={Math.round(rating)}
+        value={rating}
         allowClear={false}
         allowHalf
         onChange={value => handleChange(value)}
       />
       <p className="font-medium text-sm">
-        {!rating ? 'no rattings yet' : `Video rating ${Math.round(rating)}/5`}
+        {!rating ? 'no ratings yet' : `Video rating ${rating}/5`}
       </p>
     </div>
   );
