3dc7025bd84a5867814da2c6c9170462902e4357
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2024-01-09 21:00:40 +0000

    added the streaming prop to the ai

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,24 +1,32 @@
 import {NextResponse} from 'next/server';
-import {TbChevronCompactLeft} from 'react-icons/tb';
+import axios from 'axios';
+import {OpenAIStream, StreamingTextResponse} from 'ai';
+
 export async function POST(req: Request) {
   try {
     const body = await req.json();
     if (body.messages && body.messages.length > 0) {
       const latestQuestion = body.messages[body.messages.length - 1].content;
-      const response = await fetch(
-        // 'https://flowiseai-railway-production-1572.up.railway.app/api/v1/prediction/feef2628-4ec8-4699-b105-26cb2b7748cd',
-        'https://flowiseai-railway-production-1572.up.railway.app/api/v1/prediction/97f0ecf7-0b26-4ffc-8e20-745ed9343b21',
+      const response = await axios.post(
+        'https://flowiseai-railway-production-1572.up.railway.app/api/v1/prediction/97f0ecf7-0b26-4ffc-8e20-745ed9343b21', // ----> CHATS
         {
-          method: 'POST',
+          question: latestQuestion,
+        },
+        {
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({question: latestQuestion}),
         },
       );
-      const result = await response.json();
+
+      const result = await response.data;
       const formattedText = result.text.replace(/\n/g, '<br />');
-      return NextResponse.json(formattedText.replace(/"/g, ' '));
+
+      const stream = OpenAIStream(formattedText);
+      // Respond with the stream
+      return new StreamingTextResponse(stream);
+
+      // return NextResponse.json(formattedText.replace(/"/g, ' '));
     } else {
       return NextResponse.json('Please ask a question');
     }
