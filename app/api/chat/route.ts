3378455fdb70b58887b7a78445dac993ede56244
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2024-01-09 21:10:21 +0000

    removed the ai related stuff, was facing some production issues

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,14 +1,12 @@
 import {NextResponse} from 'next/server';
 import axios from 'axios';
-import {OpenAIStream, StreamingTextResponse} from 'ai';
-
 export async function POST(req: Request) {
   try {
     const body = await req.json();
     if (body.messages && body.messages.length > 0) {
       const latestQuestion = body.messages[body.messages.length - 1].content;
       const response = await axios.post(
-        'https://flowiseai-railway-production-1572.up.railway.app/api/v1/prediction/97f0ecf7-0b26-4ffc-8e20-745ed9343b21', // ----> CHATS
+        'https://flowiseai-railway-production-1572.up.railway.app/api/v1/prediction/97f0ecf7-0b26-4ffc-8e20-745ed9343b21',
         {
           question: latestQuestion,
         },
@@ -22,11 +20,7 @@ export async function POST(req: Request) {
       const result = await response.data;
       const formattedText = result.text.replace(/\n/g, '<br />');
 
-      const stream = OpenAIStream(formattedText);
-      // Respond with the stream
-      return new StreamingTextResponse(stream);
-
-      // return NextResponse.json(formattedText.replace(/"/g, ' '));
+      return NextResponse.json(formattedText.replace(/"/g, ' '));
     } else {
       return NextResponse.json('Please ask a question');
     }
