a824e75ea6a5466d0ffec6b0e2ba7c8450854c28
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2024-01-21 16:31:11 +0000

    feat: changed the filter for the abbreviations

--- app/(dashboard)/(routes)/uploader/analytics/_components/data-table.tsx
+++ app/(dashboard)/(routes)/uploader/analytics/_components/data-table.tsx
@@ -59,12 +59,12 @@ export function DataTable<TData, TValue>({
     <div>
       <div className="flex items-center py-4 justify-between">
         <Input
-          placeholder="Filter name..."
+          placeholder="Filter abbreviation..."
           value={
-            (table.getColumn('actualName')?.getFilterValue() as string) ?? ''
+            (table.getColumn('abbreviation')?.getFilterValue() as string) ?? ''
           }
           onChange={event =>
-            table.getColumn('actualName')?.setFilterValue(event.target.value)
+            table.getColumn('abbreviation')?.setFilterValue(event.target.value)
           }
           className="max-w-sm"
         />
