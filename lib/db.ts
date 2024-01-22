4773904135d0ee7064a174814aa0d10be5c8b102
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2024-01-22 17:20:06 +0000

    bug: reverted the db code

--- lib/db.ts
+++ lib/db.ts
@@ -1,14 +1,17 @@
 import {PrismaClient} from '@prisma/client/edge';
 import {withAccelerate} from '@prisma/extension-accelerate';
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
-export const db: any =
-  globalThis.prisma ||
-  new PrismaClient({
+function makePrisma() {
+  return new PrismaClient({
     datasources: {db: {url: process.env.DATABASE_URL}},
   }).$extends(withAccelerate());
+}
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = db;
+const globalForPrisma = global as unknown as {
+  prisma: ReturnType<typeof makePrisma>;
+};
+
+export const db = globalForPrisma.prisma ?? makePrisma();
+
+if (process.env.NODE_ENV !== 'production')
+  globalForPrisma.prisma = makePrisma();
