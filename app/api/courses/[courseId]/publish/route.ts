02c774ed24076bdbca9e61da1925709b669be982
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2024-01-24 08:28:42 +0000

    feat: we can now send emails to users on fridays reminding them to select their classes, and we can also send a reminder to them when ever a new course is published

--- app/api/courses/[courseId]/publish/route.ts
+++ app/api/courses/[courseId]/publish/route.ts
@@ -74,6 +74,38 @@ export async function PATCH(
       subject: 'Hello, 🔥',
       html: `<p> We have just published the ${courseDetail?.title} course</p> <br /> <div>Here is the link: <a href=${courseLink}>${courseDetail?.title}</a> </div> <br /> <p>Regards, <br /> meyoneducation</p>`,
     });
+
+    const usersWithoutClasses: any = [];
+    {
+      users.map(user => {
+        if (!user.publicMetadata.userClass) {
+          usersWithoutClasses.push(user.emailAddresses[0].emailAddress);
+        }
+      });
+    }
+    const reminderEmail = await transporter.sendMail({
+      from: '"meyoneducation" <meyoneducationhub@gmail.com>', // sender address
+      to: usersWithoutClasses,
+      subject: 'Hello Student',
+      html: `<p><strong>Reminder: Select Your Class on meyoneducation Platform</strong></p>
+
+    <p>Dear Student,</p>
+
+    <p>We hope this email finds you well. We would like to remind you to select your class on the meyoneducation platform. We have recently published the exciting new course, "${courseDetail?.title}," and it's essential for you to enroll in your respective class to access the course materials.</p>
+
+    <p><strong>Details:</strong></p>
+    
+    <p><strong>Course Title:</strong> ${courseDetail?.title}</p>
+    <p><strong>Course Link:</strong> <a href="${courseLink}">${courseDetail?.title}</a></p>
+
+    <p>If you're unsure how to select your class, or if you have any questions, please don't hesitate to reach out to us. You can ask for assistance in our dedicated WhatsApp group, where our team and fellow students are ready to help.</p>
+
+    <p>We appreciate your prompt attention to this matter. Your commitment to your education is valued, and we want to ensure you have the best learning experience possible.</p>
+
+    <p><strong>Best regards,</strong></p>
+    <p><em>meyoneducation Team</em></p>`,
+    });
+
     const publishedCourse = await db.course.update({
       where: {
         id: params.courseId,
--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,13 +1,14 @@
 import {clerkClient} from '@clerk/nextjs';
 import {NextResponse} from 'next/server';
+import {transporter} from '@/lib/sendEmail';
 
 export async function GET(req: Request) {
   try {
-    // if (
-    //   req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`
-    // ) {
-    //   return new NextResponse('Unauthorized', {status: 401});
-    // }
+    if (
+      req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`
+    ) {
+      return new NextResponse('Unauthorized', {status: 401});
+    }
     const users = await clerkClient.users.getUserList({
       limit: 499,
     });
@@ -21,7 +22,22 @@ export async function GET(req: Request) {
       });
     }
 
-    console.log('[USER]', usersWithoutClasses);
+    const info = await transporter.sendMail({
+      from: '"meyoneducation" <meyoneducationhub@gmail.com>', // sender address
+      to: usersWithoutClasses,
+      subject: 'Hello',
+      html: `    <p><strong>Reminder: Select Your Class on meyoneducation Platform</strong></p>
+
+    <p>Dear Student,</p>
+
+    <p>We hope this email finds you well. We would like to remind you to select your class on the meyoneducation platform. We have recently published the exciting new course, and it's essential for you to enroll in your respective class to access the course materials.</p>
+    <p>If you're unsure how to select your class, or if you have any questions, please don't hesitate to reach out to us. You can ask for assistance in our dedicated WhatsApp group, where our team and fellow students are ready to help.</p>
+
+    <p>We appreciate your prompt attention to this matter. Your commitment to your education is valued, and we want to ensure you have the best learning experience possible.</p>
+
+    <p><strong>Best regards,</strong></p>
+    <p><em>meyoneducation Team</em></p>`,
+    });
 
     return NextResponse.json({ok: true});
   } catch (error) {
