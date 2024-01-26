085aad5c0432ccd4917db3481873c0a8078eb3a4
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2024-01-26 23:28:32 +0000

    the manner of sending mails was changed, instead of mass mails, we shall be sending one per user

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -16,29 +16,32 @@ export async function GET(req: Request) {
 
     const usersWithoutClasses: any = [];
     {
-      response?.data?.map((user: any) => {
-        if (!user?.publicMetadata?.userClass) {
-          usersWithoutClasses?.push(user?.email_addresses[0].email_address);
+      response.data.map((user: any) => {
+        if (!user.publicMetadata.userClass) {
+          usersWithoutClasses.push(user.email_addresses[0].email_address);
         }
       });
     }
+    {
+      usersWithoutClasses.map(async (email: any) => {
+        const info = await transporter.sendMail({
+          from: '"meyoneducation" <meyoneducationhub@gmail.com>', // sender address
+          to: email,
+          subject: 'Hello',
+          html: `    <p><strong>Reminder: Select Your Class on meyoneducation Platform</strong></p>
 
-    const info = await transporter.sendMail({
-      from: '"meyoneducation" <meyoneducationhub@gmail.com>', // sender address
-      to: usersWithoutClasses,
-      subject: 'Hello',
-      html: `    <p><strong>Reminder: Select Your Class on meyoneducation Platform</strong></p>
+          <p>Dear Student,</p>
 
-    <p>Dear Student,</p>
+          <p>We hope this email finds you well. We would like to remind you to select your class on the meyoneducation platform. We have recently published the exciting new course, and it's essential for you to enroll in your respective class to access the course materials.</p>
+          <p>If you're unsure how to select your class, or if you have any questions, please don't hesitate to reach out to us. You can ask for assistance in our dedicated WhatsApp group, where our team and fellow students are ready to help.</p>
 
-    <p>We hope this email finds you well. We would like to remind you to select your class on the meyoneducation platform. We have recently published the exciting new course, and it's essential for you to enroll in your respective class to access the course materials.</p>
-    <p>If you're unsure how to select your class, or if you have any questions, please don't hesitate to reach out to us. You can ask for assistance in our dedicated WhatsApp group, where our team and fellow students are ready to help.</p>
+          <p>We appreciate your prompt attention to this matter. Your commitment to your education is valued, and we want to ensure you have the best learning experience possible.</p>
 
-    <p>We appreciate your prompt attention to this matter. Your commitment to your education is valued, and we want to ensure you have the best learning experience possible.</p>
-
-    <p><strong>Best regards,</strong></p>
-    <p><em>meyoneducation Team</em></p>`,
-    });
+        <p><strong>Best regards,</strong></p>
+        <p><em>meyoneducation Team</em></p>`,
+        });
+      });
+    }
 
     return NextResponse.json('Emails sent successfully', {status: 200});
   } catch (error) {
