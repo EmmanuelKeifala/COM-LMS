3b5fab140adba15cd039c190b0a1d1c9f1780f76
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2024-02-02 08:06:05 +0000

    feat: you can now send emails to set of users from the uploader mode

---
+++ app/(dashboard)/(routes)/uploader/email/_components/emailForm.tsx
@@ -0,0 +1,140 @@
+'use client';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {useForm} from 'react-hook-form';
+import {z} from 'zod';
+import {Editor} from '@/components/editor';
+import {LoadingOutlined} from '@ant-design/icons';
+import {Spin} from 'antd';
+import {Button} from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
+import {Input} from '@/components/ui/input';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import {useState} from 'react';
+
+const formSchema = z.object({
+  subject: z.string().min(2, {
+    message: 'Subject must be at least 2 characters.',
+  }),
+  messageBody: z.string().min(5, {
+    message: 'Message body must be at least 5 characters.',
+  }),
+  userCategory: z.string().min(5, {
+    message: 'select a user category',
+  }),
+});
+
+export default function EmailForm() {
+  const [isLoading, setIsLoading] = useState(false);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      subject: '',
+      messageBody: '',
+      userCategory: '',
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    // You can handle the email sending logic here
+    try {
+      setIsLoading(true);
+      await axios.post('/api/mail', {
+        data,
+      });
+      form.reset();
+      toast.success('Emails were sent successfully');
+    } catch (error) {
+      console.log(error);
+      toast.error('Something went wrong');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        className="w-full space-y-8 flex  flex-col  "
+        onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="flex flex-col-reverse mt-2 gap-y-6">
+          <FormField
+            control={form.control}
+            name="subject"
+            render={({field}) => (
+              <FormItem className="w-full">
+                <FormLabel>Subject</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter subject" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="userCategory"
+            render={({field}) => (
+              <FormItem>
+                <FormLabel>Category</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}>
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a user category" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="general">General</SelectItem>
+                    <SelectItem value="noClass">
+                      Students without class
+                    </SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <FormField
+          control={form.control}
+          name="messageBody"
+          render={({field}) => (
+            <FormItem>
+              <FormLabel>Message Body</FormLabel>
+              <FormControl>
+                <Editor {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="space-x-4">
+          <Button type="submit" disabled={isLoading}>
+            Send Email
+          </Button>
+          {isLoading && (
+            <Spin indicator={<LoadingOutlined style={{fontSize: 24}} spin />} />
+          )}
+        </div>
+      </form>
+    </Form>
+  );
+}
---
+++ app/api/mail/route.ts
@@ -0,0 +1,81 @@
+import {NextResponse} from 'next/server';
+import {transporter} from '@/lib/sendEmail';
+import axios from 'axios';
+
+async function sendEmailBatch(emails: any[], data: any) {
+  const emailPromises = emails.map(async (user: any) => {
+    await transporter.sendMail({
+      from: '"meyoneducation" <meyoneducationhub@gmail.com>',
+      to: user.email,
+      subject: data.subject,
+      html: `
+        <p>Dear ${user.name},</p>
+        ${data.messageBody}
+        <p><strong>Best regards,<br /> Emmanuel Keifala</strong></p>
+        <p><em>meyoneducation Team</em></p>`,
+    });
+  });
+
+  // Wait for all emails in the batch to be sent
+  await Promise.all(emailPromises);
+  //   transporter.close();
+}
+
+export async function POST(req: Request) {
+  try {
+    const {data} = await req.json();
+    const response = await axios.get(
+      `https://api.clerk.com/v1/users?limit=499`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.CLERK_SECRET_KEY}`,
+          Accept: 'application/json',
+        },
+      },
+    );
+
+    const userData: any = [
+      {name: 'Emmanuel', email: 'emmanuelkeifala@gmail.com'},
+    ];
+    // if (data.userCategory === 'general') {
+    //   response.data.forEach((user: any) => {
+    //     if (user) {
+    //       userData.push({
+    //         name: user?.first_name || 'Student',
+    //         email: user.email_addresses[0].email_address,
+    //       });
+    //     }
+    //   });
+    // } else if (data.userCategory === 'noClass') {
+    //   response.data.forEach((user: any) => {
+    //     if (!user.public_metadata.userClass) {
+    //       userData.push({
+    //         name: user?.first_name || 'Student',
+    //         email: user.email_addresses[0].email_address,
+    //       });
+    //     }
+    //   });
+    // }
+    // Set the batch size and delay between batches
+    const batchSize = 50;
+    const delayBetweenBatches = 5000;
+
+    // Send emails in batches
+    for (let i = 0; i < userData.length; i += batchSize) {
+      const batch = userData.slice(i, i + batchSize);
+
+      // Parallelize email sending within a batch
+      await sendEmailBatch(batch, data);
+
+      // Introduce a delay between batches
+      if (i + batchSize < userData.length) {
+        await new Promise(resolve => setTimeout(resolve, delayBetweenBatches));
+      }
+    }
+
+    return NextResponse.json('Emails sent successfully', {status: 200});
+  } catch (error) {
+    console.log('[CRON JOB ERROR]', error);
+    return new NextResponse('Internal server error', {status: 500});
+  }
+}
