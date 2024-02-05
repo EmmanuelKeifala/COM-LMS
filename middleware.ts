a8be7979296cd432bc97772c8731e2244641db7f
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2024-02-05 23:08:58 +0000

    bug_fix: added the after auth to handle edge signin cases

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,19 @@
-import {authMiddleware} from '@clerk/nextjs';
+import {authMiddleware, redirectToSignIn} from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ['/api/uploadthing', '/api/cron'],
+  publicRoutes: ['/api/uploadthing'],
+  afterAuth(auth, req, evt) {
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({returnBackUrl: req.url});
+    }
+    if (auth.userId && !auth.isPublicRoute) {
+      return NextResponse.next();
+    }
+  },
 });
 
 export const config = {
