9120e4daca5e3b07ace49185487a601da0daafe9
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2024-03-15 14:09:49 +0000

    upcoming and ended calls, logic and ui

--- app/(dashboard)/(routes)/videoStreaming/meets/_components/Button.tsx
+++ app/(dashboard)/(routes)/videoStreaming/meets/_components/Button.tsx
@@ -4,15 +4,8 @@ export default function Button({
   className,
   ...props
 }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
-  return (
-    <button
-      {...props}
-      className={cn(
-        'flex items-center justify-center gap-2 rounded-full bg-blue-500 px-3 py-2 font-semibold text-white transition-colors hover:bg-blue-600 active:bg-blue-600 disabled:bg-gray-200',
-        className,
-      )}
-    />
-  );
+  return <button {...props} className={cn(buttonClassname, className)} />;
 }
 
-//"w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+export const buttonClassname =
+  'flex items-center justify-center gap-2 rounded-full bg-blue-500 px-3 py-2 font-semibold text-white transition-colors hover:bg-blue-600 active:bg-blue-600 disabled:bg-gray-200';
--- app/(dashboard)/(routes)/videoStreaming/meets/meeting/[id]/MeetingPage.tsx
+++ app/(dashboard)/(routes)/videoStreaming/meets/meeting/[id]/MeetingPage.tsx
@@ -1,4 +1,7 @@
 'use client';
+import useLoadCall from '@/hooks/use-load-call';
+import useStreamCall from '@/hooks/use-stream-call';
+import {useUser} from '@clerk/nextjs';
 import {
   Call,
   SpeakerLayout,
@@ -6,6 +9,7 @@ import {
   StreamTheme,
   useStreamVideoClient,
   CallControls,
+  useCallStateHooks,
 } from '@stream-io/video-react-sdk';
 import {Loader2} from 'lucide-react';
 import {useState} from 'react';
@@ -14,10 +18,10 @@ interface MeetingPageProps {
   id: string;
 }
 export default function MeetingPage({id}: MeetingPageProps) {
-  const [call, setCall] = useState<Call>();
-  const client = useStreamVideoClient();
+  const {user, isLoaded: userLoaded} = useUser();
+  const {call, callLoading} = useLoadCall(id);
 
-  if (!client) {
+  if (!userLoaded || callLoading) {
     return (
       <>
         <Loader2 className="mt-10 mx-auto animate-spin" />
@@ -25,23 +29,71 @@ export default function MeetingPage({id}: MeetingPageProps) {
     );
   }
   if (!call) {
-    return (
-      <button
-        onClick={async () => {
-          const call = client.call('default', id);
-          await call.join();
-          setCall(call);
-        }}>
-        Join Meeting
-      </button>
-    );
+    return <p className="text-center font-bold">Call not found</p>;
   }
+
+  const notAllowedToJoin =
+    call.type === 'private-meeting' &&
+    (!user || !call.state.members.find(m => m.user.id === user.id));
+
+  if (notAllowedToJoin) {
+    return <p className="text-center font-bold">You are not allowed to Join</p>;
+  }
+
   return (
     <StreamCall call={call}>
       <StreamTheme className="space-y-7 mt-10 ">
-        <SpeakerLayout />
-        <CallControls />
+        <MeetingScreen />
       </StreamTheme>
     </StreamCall>
+  );
+}
+
+function MeetingScreen() {
+  const {useCallEndedAt, useCallStartsAt} = useCallStateHooks();
+
+  const callEndedAt = useCallEndedAt();
+  const callStartsAt = useCallStartsAt();
+
+  const callIsInFuture = callStartsAt && new Date(callStartsAt) > new Date();
+
+  const callHasEnded = !!callEndedAt;
+
+  if (callHasEnded) {
+    return <MeetingEndedScreen />;
+  }
+  if (callIsInFuture) {
+    return <UpcomingMeetingsScreen />;
+  }
+
+  return <div>Call UI</div>;
+}
+
+function UpcomingMeetingsScreen() {
+  const call = useStreamCall();
+
+  return (
+    <div className="flex flex-col items-center gap-6">
+      <p>
+        This meeting has not started yet. It will start at{''}
+        <span className="font-bold">
+          {call.state.startsAt?.toLocaleString()}
+        </span>
+      </p>
+
+      {call.state.custom.description && (
+        <p>
+          Description: {''}
+          <span className="font-bold">{call.state.custom.description}</span>
+        </p>
+      )}
+    </div>
+  );
+}
+function MeetingEndedScreen() {
+  return (
+    <div className="flex flex-col items-center gap-6">
+      <p className="font-bold">This meeting has ended</p>
+    </div>
   );
 }
