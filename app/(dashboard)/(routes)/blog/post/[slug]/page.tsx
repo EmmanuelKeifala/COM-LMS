8aa39e26085475965a1fc9fb76ea96e68558f4fc
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2024-03-30 10:47:42 +0000

    change the ui <hr /> a bit

--- app/(dashboard)/(routes)/blog/post/[slug]/page.tsx
+++ app/(dashboard)/(routes)/blog/post/[slug]/page.tsx
@@ -42,7 +42,7 @@ async function Post({params: {slug}}: Props) {
           <div className="absolute top-0 w-full h-full opacity-10 blur-sm p-10">
             <Image
               className="object-cover object-center mx-auto"
-              src={urlForImage(post.mainImage)}
+              src={urlForImage(post?.mainImage)}
               alt={post.title}
               fill
             />
@@ -50,7 +50,7 @@ async function Post({params: {slug}}: Props) {
           <section className="p-5 bg-slate-200 w-full">
             <div className="flex flex-col md:flex-row justify-between gap-y-5">
               <div>
-                <h1 className="text-4xl font-extrabold">{post.title}</h1>
+                <h1 className="text-4xl font-extrabold">{post?.title}</h1>
                 <p className="">
                   {new Date(post._createdAt).toLocaleDateString('en-US', {
                     day: 'numeric',
@@ -62,13 +62,13 @@ async function Post({params: {slug}}: Props) {
               <div className="flex items-center space-x-2">
                 <Image
                   className="rounded-full"
-                  src={urlForImage(post.author.image)}
+                  src={urlForImage(post?.author?.image)}
                   alt={post.author.name}
                   height={40}
                   width={40}
                 />
                 <div className="w-64">
-                  <h3 className="text-lg font-bold">{post.author.name}</h3>
+                  <h3 className="text-lg font-bold">{post?.author?.name}</h3>
                   <div>
                     <PortableText
                       value={post.author.bio}
@@ -80,13 +80,13 @@ async function Post({params: {slug}}: Props) {
             </div>
 
             <div>
-              <h2 className="italic pt-10">{post.description}</h2>
+              <h2 className="italic pt-10">{post?.description}</h2>
               <div className="flex items-center justify-end mt-auto space-x-2">
                 {post.categories.map(category => (
                   <p
                     key={category.title}
                     className="bg-gray-800 text-white px-3 py-1 rounded-full text-sm font-semibold mt-4">
-                    {category.title}
+                    {category?.title}
                   </p>
                 ))}
               </div>
@@ -94,7 +94,7 @@ async function Post({params: {slug}}: Props) {
           </section>
         </div>
       </section>
-      <PortableText value={post.body} components={RichTextComponent} />
+      <PortableText value={post?.body} components={RichTextComponent} />
     </article>
   );
 }
