9208b8e7392fe84f61b1891d0d6abd6f9648a442
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2024-03-30 11:46:48 +0000

    attempted revalidation

--- app/(dashboard)/(routes)/blog/page.tsx
+++ app/(dashboard)/(routes)/blog/page.tsx
@@ -1,7 +1,9 @@
+'use client';
 import {client} from '@/sanity/lib/client';
 import {groq} from 'next-sanity';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import BlogList from './_components/BlogList';
+
 type Props = {};
 
 const query = groq`*[_type=="post"]{
@@ -10,8 +12,21 @@ const query = groq`*[_type=="post"]{
   categories[]->
 }|order(_createdAt desc)`;
 
-const BlogPage = async (props: Props) => {
-  const posts = await client.fetch(query);
+const BlogPage = (props: Props) => {
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = async () => {
+    const fetchedPosts = await client.fetch(query);
+    setPosts(fetchedPosts);
+  };
+
+  useEffect(() => {
+    fetchPosts(); // Fetch posts initially
+    const intervalId = setInterval(fetchPosts, 60000); // Fetch posts every 60 seconds
+
+    return () => clearInterval(intervalId); // Cleanup function to clear interval
+  }, []); // Empty dependency array to run effect only once on mount
+
   return (
     <div>
       <BlogList posts={posts} />
